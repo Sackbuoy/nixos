c14db8e8ea05dfaed6144a35524c6ecd35ca47b4
Sackbuoy <cameronkientz@proton.me>
2025-01-03 11:59:01 -0600

    updating git config

--- sackbuoy/modules/git/git.nix
+++ sackbuoy/modules/git/git.nix
@@ -1,10 +1,18 @@
 {
   programs.git = {
     enable = true;
+    delta.enable = true;
     userName = "Sackbuoy";
     userEmail = "cameronkientz@proton.me";
     aliases = {
       co = "checkout";
+    };
+    extraConfig = {
+      url = {
+        "ssh://git@github.com" = {
+          insteadOf = "https://github.com";
+        };
+      };
     };
   };
 }
