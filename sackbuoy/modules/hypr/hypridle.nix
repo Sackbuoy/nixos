5adcfb0f64bbe2bad62bc7551ad6506134a5547f
Sackbuoy <cameronkientz@proton.me>
2025-01-07 10:57:56 -0600

    updating hypridle timeouts and monitor config for hyprland

--- sackbuoy/modules/hypr/hypridle.nix
+++ sackbuoy/modules/hypr/hypridle.nix
@@ -10,13 +10,11 @@
 
       listener = [
         {
-          # timeout = 900;
-          timeout = 60;
+          timeout = 900;
           on-timeout = "hyprlock";
         }
         {
-          # timeout = 1200;
-          timeout = 120;
+          timeout = 1200;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
