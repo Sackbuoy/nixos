4b45237a2bbc4db5ade00cfbda2c9f77eac8235b
Sackbuoy <cameronkientz@proton.me>
2025-05-04 10:45:28 -0500

    improve zsh startup time and retain path tmux

--- sackbuoy/home.nix
+++ sackbuoy/home.nix
@@ -143,7 +143,7 @@
       bindkey '^R' history-incremental-search-backward
       export PATH=/home/sackbuoy/.bin:$PATH
 
-      (
+      ((
         set -x; cd "''$(mktemp -d)" &&
         OS="''$(uname | tr '[:upper:]' '[:lower:]')" &&
         ARCH="''$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64''$/arm64/')" &&
@@ -151,7 +151,7 @@
         curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/''${KREW}.tar.gz" &&
         tar zxvf "''${KREW}.tar.gz" &&
         ./"''${KREW}" install krew
-      ) &>/dev/null
+      ) & disown) &>/dev/null
 
       export PATH="''${KREW_ROOT:-''$HOME/.krew}/bin:''$PATH"
 
