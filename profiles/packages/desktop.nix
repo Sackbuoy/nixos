736b86c5f9be93da7e7d1467dcd6ef031c4f9521
Sackbuoy <cameronkientz@proton.me>
2026-04-09 18:02:07 -0500

    fix discord scaling issue

--- profiles/packages/desktop.nix
+++ profiles/packages/desktop.nix
@@ -1,12 +1,25 @@
 # Desktop applications (Linux only)
 {pkgs}:
+let
+  discord-wrapped = pkgs.symlinkJoin {
+    name = "discord-wrapped";
+    paths = [ pkgs.discord ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/discord \
+        --add-flags "--force-device-scale-factor=1"
+      wrapProgram $out/bin/Discord \
+        --add-flags "--force-device-scale-factor=1"
+    '';
+  };
+in
 pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux (
   with pkgs; [
     signal-desktop
     protonmail-desktop
     electron-mail
     spotify-player
-    discord
+    discord-wrapped
     zoom-us
     slack
     spotify
